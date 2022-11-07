66f098993574500597d36a5ac20866630b4a3c10
dmrasf <dmrasf@qq.com>
2022-11-07 11:45:22 +0800

    test: 02-impl-debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro::TokenStream;
 use proc_macro2;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
@@ -12,6 +13,38 @@ pub fn derive(input: TokenStream) -> TokenStream {
 }
 
 fn expand(st: &DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
-    let ret = proc_macro2::TokenStream::new();
+    let struct_ident = &st.ident;
+    let fields = get_struct_fields(st)?;
+
+    let mut fmt_debug_body = proc_macro2::TokenStream::new();
+    for field in fields.iter() {
+        let field_ident = &field.ident;
+        fmt_debug_body.extend(quote!(
+            .field(stringify!(#field_ident), &self.#field_ident)
+        ));
+    }
+
+    let ret = quote!(
+        impl std::fmt::Debug for #struct_ident {
+            fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+                fmt.debug_struct(stringify!(#struct_ident))
+                    #fmt_debug_body
+                    .finish()
+            }
+        }
+    );
     Ok(ret)
+}
+
+fn get_struct_fields(
+    st: &DeriveInput,
+) -> syn::Result<&syn::punctuated::Punctuated<syn::Field, syn::Token![,]>> {
+    if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = st.data
+    {
+        return Ok(named);
+    }
+    Err(syn::Error::new_spanned(st, "miss field"))
 }
