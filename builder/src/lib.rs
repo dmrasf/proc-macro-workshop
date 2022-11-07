164b77e4f12ce3fd3cc4fcd9e1aa544924823173
dmrasf <dmrasf@qq.com>
2022-11-07 15:32:17 +0800

    test: 01-parse

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,17 @@
 use proc_macro::TokenStream;
+use proc_macro2;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let st = parse_macro_input!(input as DeriveInput);
+    match expand(&st) {
+        Ok(token) => token.into(),
+        Err(e) => e.to_compile_error().into(),
+    }
+}
 
-    unimplemented!()
+fn expand(st: &DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    let ret = proc_macro2::TokenStream::new();
+    Ok(ret)
 }
