a73cd39a3689a659113207cd003422cc8d21d782
dmrasf <dmrasf@qq.com>
2022-11-07 11:47:39 +0800

    test: 03-custom-format

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -3,7 +3,7 @@ use proc_macro2;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let st = parse_macro_input!(input as DeriveInput);
     match expand(&st) {
@@ -19,9 +19,14 @@ fn expand(st: &DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let mut fmt_debug_body = proc_macro2::TokenStream::new();
     for field in fields.iter() {
         let field_ident = &field.ident;
-        fmt_debug_body.extend(quote!(
-            .field(stringify!(#field_ident), &self.#field_ident)
-        ));
+        match get_debug_attr_name(field)? {
+            Some(s) => fmt_debug_body.extend(quote!(
+                .field(stringify!(#field_ident), &format_args!(#s, self.#field_ident))
+            )),
+            None => fmt_debug_body.extend(quote!(
+                .field(stringify!(#field_ident), &self.#field_ident)
+            )),
+        }
     }
 
     let ret = quote!(
@@ -47,4 +52,24 @@ fn get_struct_fields(
         return Ok(named);
     }
     Err(syn::Error::new_spanned(st, "miss field"))
+}
+
+fn get_debug_attr_name(field: &syn::Field) -> syn::Result<Option<String>> {
+    for attr in field.attrs.iter() {
+        if let Ok(syn::Meta::NameValue(syn::MetaNameValue {
+            ref path, ref lit, ..
+        })) = attr.parse_meta()
+        {
+            if let Some(p) = path.segments.first() {
+                if p.ident == "debug" {
+                    if let syn::Lit::Str(ref lit_str) = lit {
+                        return Ok(Some(lit_str.value()));
+                    }
+                } else {
+                    return Err(syn::Error::new_spanned(p, r#"expected `debug = ""`"#));
+                }
+            }
+        }
+    }
+    Ok(None)
 }
