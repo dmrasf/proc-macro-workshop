4e425fc74a764b7587341ed56c0e40962e289c15
dmrasf <dmrasf@qq.com>
2022-11-10 08:24:54 +0800

    sorted: 01-parse-enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,17 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, Item};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+    let st = parse_macro_input!(input as Item);
 
-    unimplemented!()
+    match expand(&st) {
+        Ok(ts) => ts.into(),
+        Err(e) => e.to_compile_error().into(),
+    }
+}
+
+fn expand(st: &Item) -> syn::Result<proc_macro2::TokenStream> {
+    let ret = proc_macro2::TokenStream::new();
+    Ok(ret)
 }
