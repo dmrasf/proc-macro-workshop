5bc4a9d694f0fdc5487ff91aeb6cc9a718e667f4
dmrasf <dmrasf@qq.com>
2022-11-10 11:06:30 +0800

    sorted: 05-match-expr

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,7 +1,11 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::ToTokens;
-use syn::{parse_macro_input, Item, ItemEnum};
+use syn::{
+    parse_macro_input,
+    visit_mut::{self, VisitMut},
+    Item, ItemEnum, ItemFn,
+};
 
 #[proc_macro_attribute]
 pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
@@ -48,4 +52,78 @@ fn check_order(eu: &ItemEnum) -> syn::Result<proc_macro2::TokenStream> {
     }
 
     Ok(eu.to_token_stream())
+}
+
+#[proc_macro_attribute]
+pub fn check(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut st = parse_macro_input!(input as ItemFn);
+
+    match match_expand(&mut st) {
+        Ok(ts) => ts.into(),
+        Err(e) => {
+            let mut ts = e.to_compile_error();
+            ts.extend(st.to_token_stream());
+            ts.into()
+        }
+    }
+}
+
+struct MatchVisitor {
+    err: Option<syn::Error>,
+}
+impl VisitMut for MatchVisitor {
+    fn visit_expr_match_mut(&mut self, i: &mut syn::ExprMatch) {
+        let mut remove_idx: isize = -1;
+        for (idx, attr) in i.attrs.iter().enumerate() {
+            if let Some(ps) = attr.path.segments.first() {
+                if ps.ident.to_string() == "sorted" {
+                    remove_idx = idx as isize;
+                    break;
+                }
+            }
+        }
+
+        if remove_idx != -1 {
+            i.attrs.remove(remove_idx as usize);
+
+            let mut ori_idents = Vec::new();
+            for arm in &i.arms {
+                if let syn::Pat::TupleStruct(syn::PatTupleStruct { ref path, .. }) = arm.pat {
+                    if let Some(ps) = path.segments.first() {
+                        ori_idents.push(&ps.ident);
+                    }
+                }
+            }
+
+            let mut sorted_idents = ori_idents.clone();
+            sorted_idents.sort_by(|a, b| a.to_string().cmp(&b.to_string()));
+
+            for (ori, sorted) in ori_idents.iter().zip(sorted_idents.iter()) {
+                if ori != sorted {
+                    self.err = Some(syn::Error::new_spanned(
+                        sorted.to_token_stream(),
+                        format!(
+                            "{} should sort before {}",
+                            sorted.to_string(),
+                            ori.to_string()
+                        ),
+                    ));
+                    return;
+                }
+            }
+        }
+
+        visit_mut::visit_expr_match_mut(self, i);
+    }
+}
+
+fn match_expand(st: &mut ItemFn) -> syn::Result<proc_macro2::TokenStream> {
+    let mut visitor = MatchVisitor { err: None };
+    visitor.visit_item_fn_mut(st);
+
+    if visitor.err.is_none() {
+        syn::Result::Ok(st.to_token_stream())
+    } else {
+        syn::Result::Err(visitor.err.unwrap())
+    }
 }
